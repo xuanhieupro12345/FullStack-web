9d78c85bf0fea828b77e686f777a066d021543a6
xuanhieu12345 <hieu94842@donga.edu.vn>
2023-03-20 08:57:22 +0700

    reactjs video34

--- src/containers/Auth/login.js
+++ src/containers/Auth/login.js
@@ -9,10 +9,47 @@ import { FormattedMessage } from 'react-intl';
 
 class Login extends Component {
     constructor(props) {
-        super(props);
+        super(props);// dùng hàm này có thế kế  thừa được
+        this.state = {
+            username: '',
+            password: 'xuanhieu',
+            isShowPassword: false
+
+        }
+    }
+    handleOnChangeUserName = (event) => {
+        //gõ chữ thay đổi trên input
+        this.setState({
+            username: event.target.value
+        })
+        console.log(event.target.value)
     }
 
+    handleOnChangePassword = (event) => {
+        //gõ chữ thay đổi trên input
+        this.setState({
+            password: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    //log ra du lieu nguoi login
+    handleLogin = () => {
+        console.log('userName :', this.state.username, 'password : ', this.state.password)
+        console.log('all state :', this.state)
+    }
+
+    //hiểu thị mật khẩu người dùng
+    handleShowHidePassword = () => {
+        this.setState({
+            // user ! thì lấy giá trị ngược lại
+            isShowPassword: !this.state.isShowPassword
+        })
+    }
     render() {
+
+
+
         return (
             <div className='login-background'>
                 <div className='login-container'>
@@ -20,14 +57,32 @@ class Login extends Component {
                         <div className='clo-12 text-login'>Login</div>
                         <div className='clo-12 form-group login-input '>
                             <label>UserName</label>
-                            <input type='text' className='form-control' placeholder='Enter your username' />
+                            <input type='text' className='form-control' placeholder='Enter your username'
+                                value={this.state.username} // truyền giá trị cho name
+                                //gõ được thay đổi trong hàm input
+                                onChange={(event) => this.handleOnChangeUserName(event)} />
+
                         </div>
                         <div className='clo-12 form-group login-input '>
                             <label>Password</label>
-                            <input type='password' className='form-control' placeholder='Enter your password' />
+                            <div className='custom-input-password'>
+                                <input type={this.state.isShowPassword ? 'text' : 'password'}
+                                    className='form-control' placeholder='Enter your password'
+                                    value={this.state.password} // truyền giá trị cho name
+                                    //gõ được thay đổi trong hàm input
+                                    onChange={(event) => this.handleOnChangePassword(event)}
+                                />
+                                <span onClick={() => { this.handleShowHidePassword() }}>
+                                    <i class={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
+                                </span>
+
+
+                            </div>
+
                         </div>
                         <div className='clo-12 '>
-                            <button className='btn-login' >Login</button>
+                            {/* onClick={() => { this.handleLogin() }} lấy được dữ liệu */}
+                            <button className='btn-login' onClick={() => { this.handleLogin() }} >Login</button>
                         </div>
                         <div className='clo-12'>
                             <span className='forgot-passwod'>Forgot your password?</span>
